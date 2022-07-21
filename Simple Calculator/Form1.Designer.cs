88f28d2d95bc4c9c26da0d585e0641b5314b241e
Veli <Veli@test.com>
2022-07-21 11:47:38 +0300

    Add project files.

---
+++ Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.temizleButton = new System.Windows.Forms.Button();
+            this.artiButton = new System.Windows.Forms.Button();
+            this.esittirButton = new System.Windows.Forms.Button();
+            this.boluButton = new System.Windows.Forms.Button();
+            this.carpiButton = new System.Windows.Forms.Button();
+            this.eksiButton = new System.Windows.Forms.Button();
+            this.rakam0Button = new System.Windows.Forms.Button();
+            this.rakam9Button = new System.Windows.Forms.Button();
+            this.rakam8Button = new System.Windows.Forms.Button();
+            this.rakakm7Button = new System.Windows.Forms.Button();
+            this.rakam6Button = new System.Windows.Forms.Button();
+            this.rakam5Button = new System.Windows.Forms.Button();
+            this.rakam4Button = new System.Windows.Forms.Button();
+            this.rakam3Button = new System.Windows.Forms.Button();
+            this.rakam2Button = new System.Windows.Forms.Button();
+            this.rakam1Button = new System.Windows.Forms.Button();
+            this.ekranLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.temizleButton);
+            this.panel1.Controls.Add(this.artiButton);
+            this.panel1.Controls.Add(this.esittirButton);
+            this.panel1.Controls.Add(this.boluButton);
+            this.panel1.Controls.Add(this.carpiButton);
+            this.panel1.Controls.Add(this.eksiButton);
+            this.panel1.Controls.Add(this.rakam0Button);
+            this.panel1.Controls.Add(this.rakam9Button);
+            this.panel1.Controls.Add(this.rakam8Button);
+            this.panel1.Controls.Add(this.rakakm7Button);
+            this.panel1.Controls.Add(this.rakam6Button);
+            this.panel1.Controls.Add(this.rakam5Button);
+            this.panel1.Controls.Add(this.rakam4Button);
+            this.panel1.Controls.Add(this.rakam3Button);
+            this.panel1.Controls.Add(this.rakam2Button);
+            this.panel1.Controls.Add(this.rakam1Button);
+            this.panel1.Controls.Add(this.ekranLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(396, 448);
+            this.panel1.TabIndex = 0;
+            // 
+            // temizleButton
+            // 
+            this.temizleButton.BackColor = System.Drawing.Color.Red;
+            this.temizleButton.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.temizleButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.temizleButton.Location = new System.Drawing.Point(12, 363);
+            this.temizleButton.Name = "temizleButton";
+            this.temizleButton.Size = new System.Drawing.Size(75, 75);
+            this.temizleButton.TabIndex = 17;
+            this.temizleButton.Text = "C";
+            this.temizleButton.UseVisualStyleBackColor = false;
+            this.temizleButton.Click += new System.EventHandler(this.temizleButton_Click);
+            // 
+            // artiButton
+            // 
+            this.artiButton.BackColor = System.Drawing.Color.Blue;
+            this.artiButton.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.artiButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.artiButton.Location = new System.Drawing.Point(308, 118);
+            this.artiButton.Name = "artiButton";
+            this.artiButton.Size = new System.Drawing.Size(75, 75);
+            this.artiButton.TabIndex = 16;
+            this.artiButton.Text = "+";
+            this.artiButton.UseVisualStyleBackColor = false;
+            this.artiButton.Click += new System.EventHandler(this.artiButton_Click);
+            // 
+            // esittirButton
+            // 
+            this.esittirButton.BackColor = System.Drawing.Color.Lime;
+            this.esittirButton.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.esittirButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.esittirButton.Location = new System.Drawing.Point(213, 363);
+            this.esittirButton.Name = "esittirButton";
+            this.esittirButton.Size = new System.Drawing.Size(75, 75);
+            this.esittirButton.TabIndex = 15;
+            this.esittirButton.Text = "=";
+            this.esittirButton.UseVisualStyleBackColor = false;
+            this.esittirButton.Click += new System.EventHandler(this.esittirButton_Click);
+            // 
+            // boluButton
+            // 
+            this.boluButton.BackColor = System.Drawing.Color.Blue;
+            this.boluButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.boluButton.Font = new System.Drawing.Font("Segoe UI Emoji", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.boluButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.boluButton.Location = new System.Drawing.Point(308, 363);
+            this.boluButton.Name = "boluButton";
+            this.boluButton.Size = new System.Drawing.Size(75, 75);
+            this.boluButton.TabIndex = 13;
+            this.boluButton.Text = "/";
+            this.boluButton.UseVisualStyleBackColor = false;
+            this.boluButton.Click += new System.EventHandler(this.boluButton_Click);
+            // 
+            // carpiButton
+            // 
+            this.carpiButton.BackColor = System.Drawing.Color.Blue;
+            this.carpiButton.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.carpiButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.carpiButton.Location = new System.Drawing.Point(308, 280);
+            this.carpiButton.Name = "carpiButton";
+            this.carpiButton.Size = new System.Drawing.Size(75, 75);
+            this.carpiButton.TabIndex = 12;
+            this.carpiButton.Text = "x";
+            this.carpiButton.UseVisualStyleBackColor = false;
+            this.carpiButton.Click += new System.EventHandler(this.carpiButton_Click);
+            // 
+            // eksiButton
+            // 
+            this.eksiButton.BackColor = System.Drawing.Color.Blue;
+            this.eksiButton.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.eksiButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.eksiButton.Location = new System.Drawing.Point(308, 199);
+            this.eksiButton.Name = "eksiButton";
+            this.eksiButton.Size = new System.Drawing.Size(75, 75);
+            this.eksiButton.TabIndex = 11;
+            this.eksiButton.Text = "-";
+            this.eksiButton.UseVisualStyleBackColor = false;
+            this.eksiButton.Click += new System.EventHandler(this.eksiButton_Click);
+            // 
+            // rakam0Button
+            // 
+            this.rakam0Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam0Button.Location = new System.Drawing.Point(111, 363);
+            this.rakam0Button.Name = "rakam0Button";
+            this.rakam0Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam0Button.TabIndex = 10;
+            this.rakam0Button.Text = "0";
+            this.rakam0Button.UseVisualStyleBackColor = true;
+            this.rakam0Button.Click += new System.EventHandler(this.rakam0Button_Click);
+            // 
+            // rakam9Button
+            // 
+            this.rakam9Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam9Button.Location = new System.Drawing.Point(213, 282);
+            this.rakam9Button.Name = "rakam9Button";
+            this.rakam9Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam9Button.TabIndex = 9;
+            this.rakam9Button.Text = "9";
+            this.rakam9Button.UseVisualStyleBackColor = true;
+            this.rakam9Button.Click += new System.EventHandler(this.rakam9Button_Click);
+            // 
+            // rakam8Button
+            // 
+            this.rakam8Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam8Button.Location = new System.Drawing.Point(111, 280);
+            this.rakam8Button.Name = "rakam8Button";
+            this.rakam8Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam8Button.TabIndex = 8;
+            this.rakam8Button.Text = "8";
+            this.rakam8Button.UseVisualStyleBackColor = true;
+            this.rakam8Button.Click += new System.EventHandler(this.rakam8Button_Click);
+            // 
+            // rakakm7Button
+            // 
+            this.rakakm7Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakakm7Button.Location = new System.Drawing.Point(12, 282);
+            this.rakakm7Button.Name = "rakakm7Button";
+            this.rakakm7Button.Size = new System.Drawing.Size(75, 75);
+            this.rakakm7Button.TabIndex = 7;
+            this.rakakm7Button.Text = "7";
+            this.rakakm7Button.UseVisualStyleBackColor = true;
+            this.rakakm7Button.Click += new System.EventHandler(this.rakakm7Button_Click);
+            // 
+            // rakam6Button
+            // 
+            this.rakam6Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam6Button.Location = new System.Drawing.Point(213, 199);
+            this.rakam6Button.Name = "rakam6Button";
+            this.rakam6Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam6Button.TabIndex = 6;
+            this.rakam6Button.Text = "6";
+            this.rakam6Button.UseVisualStyleBackColor = true;
+            this.rakam6Button.Click += new System.EventHandler(this.rakam6Button_Click);
+            // 
+            // rakam5Button
+            // 
+            this.rakam5Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam5Button.Location = new System.Drawing.Point(111, 199);
+            this.rakam5Button.Name = "rakam5Button";
+            this.rakam5Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam5Button.TabIndex = 5;
+            this.rakam5Button.Text = "5";
+            this.rakam5Button.UseVisualStyleBackColor = true;
+            this.rakam5Button.Click += new System.EventHandler(this.rakam5Button_Click);
+            // 
+            // rakam4Button
+            // 
+            this.rakam4Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam4Button.Location = new System.Drawing.Point(12, 199);
+            this.rakam4Button.Name = "rakam4Button";
+            this.rakam4Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam4Button.TabIndex = 4;
+            this.rakam4Button.Text = "4";
+            this.rakam4Button.UseVisualStyleBackColor = true;
+            this.rakam4Button.Click += new System.EventHandler(this.rakam4Button_Click);
+            // 
+            // rakam3Button
+            // 
+            this.rakam3Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam3Button.Location = new System.Drawing.Point(213, 118);
+            this.rakam3Button.Name = "rakam3Button";
+            this.rakam3Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam3Button.TabIndex = 3;
+            this.rakam3Button.Text = "3";
+            this.rakam3Button.UseVisualStyleBackColor = true;
+            this.rakam3Button.Click += new System.EventHandler(this.rakam3Button_Click);
+            // 
+            // rakam2Button
+            // 
+            this.rakam2Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam2Button.Location = new System.Drawing.Point(111, 118);
+            this.rakam2Button.Name = "rakam2Button";
+            this.rakam2Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam2Button.TabIndex = 2;
+            this.rakam2Button.Text = "2";
+            this.rakam2Button.UseVisualStyleBackColor = true;
+            this.rakam2Button.Click += new System.EventHandler(this.rakam2Button_Click);
+            // 
+            // rakam1Button
+            // 
+            this.rakam1Button.Font = new System.Drawing.Font("Segoe UI Emoji", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rakam1Button.Location = new System.Drawing.Point(12, 118);
+            this.rakam1Button.Name = "rakam1Button";
+            this.rakam1Button.Size = new System.Drawing.Size(75, 75);
+            this.rakam1Button.TabIndex = 1;
+            this.rakam1Button.Text = "1";
+            this.rakam1Button.UseVisualStyleBackColor = true;
+            this.rakam1Button.Click += new System.EventHandler(this.rakam1Button_Click);
+            // 
+            // ekranLabel
+            // 
+            this.ekranLabel.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ekranLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ekranLabel.Font = new System.Drawing.Font("Segoe UI Black", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ekranLabel.Location = new System.Drawing.Point(13, 9);
+            this.ekranLabel.Name = "ekranLabel";
+            this.ekranLabel.Size = new System.Drawing.Size(371, 81);
+            this.ekranLabel.TabIndex = 0;
+            this.ekranLabel.Text = "0";
+            this.ekranLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 448);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "By VELI";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button temizleButton;
+        private Button artiButton;
+        private Button esittirButton;
+        private Button boluButton;
+        private Button carpiButton;
+        private Button eksiButton;
+        private Button rakam0Button;
+        private Button rakam9Button;
+        private Button rakam8Button;
+        private Button rakakm7Button;
+        private Button rakam6Button;
+        private Button rakam5Button;
+        private Button rakam4Button;
+        private Button rakam3Button;
+        private Button rakam2Button;
+        private Button rakam1Button;
+        private Label ekranLabel;
+    }
+}
